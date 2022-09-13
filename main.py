d634fa21e903758376869af0a5bf58f5df42b4be
Keith Gaines <patrick.keith.gaines@gmail.com>
2022-09-13 16:35:19 -0500

    removed unncessary code

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 import random
 
-num = 0
-board = {num:'_' for num in range(1, 10)}
-
 def printBoard():
-    global board
+    board = {num:'_' for num in range(1, 10)}
+
     print(f"{board[1]} | {board[2]} | {board[3]}")
     print(f"{board[4]} | {board[5]} | {board[6]}")
     print(f"{board[7]} | {board[8]} | {board[9]}")
 
-
 printBoard()
+
