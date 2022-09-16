66a897c294e80c30ea07511c7708b97153ea2de9
Keith Gaines <patrick.keith.gaines@gmail.com>
2022-09-16 13:54:24 -0500

    moved function to class

--- globals.py
+++ globals.py
@@ -1,5 +1,9 @@
-class global_info:
-
-    board = {num:'_' for num in range(1, 10)}
+class GlobalInfo:
+    board = {num: '_' for num in range(1, 10)}
     openSpaces = [x for x in range(1, 10)]
     gameOn = True
+    
+    def printboard():
+        print(f"\n{GlobalInfo.board[1]} | {GlobalInfo.board[2]} | {GlobalInfo.board[3]}")
+        print(f"{GlobalInfo.board[4]} | {GlobalInfo.board[5]} | {GlobalInfo.board[6]}")
+        print(f"{GlobalInfo.board[7]} | {GlobalInfo.board[8]} | {GlobalInfo.board[9]}")
