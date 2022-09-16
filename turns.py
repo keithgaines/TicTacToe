a8dcb50626e665af2cd25f70340708d35f38b1ef
Keith Gaines <patrick.keith.gaines@gmail.com>
2022-09-16 13:54:01 -0500

    fixed bug causing endless loop

--- turns.py
+++ turns.py
@@ -4,18 +4,32 @@ from globals import GlobalInfo
 import check_win
 
 def playgame():
-    turn = 'X'
+    
     count = 0
 
     while GlobalInfo.gameOn:
+        turn = 'X'
         space = input("Please select a space: ")
         space = int(space)
-        GlobalInfo.board[space] = turn
-        try:
-            GlobalInfo.openSpaces.remove(space)
-        except ValueError:
-            print("You have selected an invalid space. Please try again. ")    
+       
+        while GlobalInfo.board[space] == 'O':
+            print("Invalid selection.")
+            space = input("Please select a space: ")
+        
+        GlobalInfo.board[space] = turn  
         GlobalInfo.printboard()
+
+        check_win.is_game_over()
+
+        turn = 'O'
+        comp_space = random.randint(1, 9)
+
+        while GlobalInfo.board[comp_space] != '_':
+            comp_space = random.randint(1, 9)
+        if GlobalInfo.board[comp_space] == '_':
+            GlobalInfo.board[comp_space] = turn
+        GlobalInfo.printboard()
+        
         if turn == 'X':
             turn = 'O'
             count += 1
@@ -24,4 +38,6 @@ def playgame():
             turn = 'X'
 
         check_win.is_game_over()
+        if GlobalInfo.gameOn == False:
+            break
         
